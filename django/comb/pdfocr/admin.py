4f467c4b14b0510b3728111c14ce6bfedf1e0151
Babak Pourkazemi <bp5xj@virginia.edu>
2014-04-13 04:43:55 +0000

    Push to pull

--- django/comb/pdfocr/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from pdfocr.models import OcrWrapper
-
-# Register your models here.
-admin.site.register(OcrWrapper)
--- django/comb/pdfocr/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class OcrWrapper(models.Model):
-	pdf = models.FileField(upload_to='pdfs')
-	
