df236b5ce76edf13227fea6b7933b2f70c9831f3
Babak Pourkazemi <bp5xj@virginia.edu>
2014-04-13 05:14:44 -0400

    Skeleton support for angular

--- comb/static/js/app.js
+++ comb/static/js/app.js
@@ -1,25 +1,39 @@
-function chooseFile() {
-	$('#choose-pdf').change(function() {
-		// File has now been selected
-		$('#pdf-button').text('Convert to Text')
-		$('#pdf-button').attr('onclick', 'submitFile();')
-		$('#change-pdf').show();
-	})
-	$("#choose-pdf").click();
-}
+var app = angular.module('compApp', []);
 
-function submitFile() {
-	$('.header').addClass('shrink');
-	$('#pdf-button').addClass('shrink');
-	$('span').addClass('shrink');
-	
-	$('#submit-pdf').click();
-	$('#pdf-button').text('Cancel');
-	$('#pdf-button').css('background', '#ea8557');
-	$('#change-pdf').hide();
-}
+app.controller('MainCtrl', function($scope) {
+	console.log('Current controller: MainCtrl');
 
-function changePDF() {
-	$('#pdf-button').attr('onclick', 'chooseFile();');
-	$('#pdf-button').click();
-}
+	function chooseFile() {
+		$('#choose-pdf').change(function() {
+			// File has now been selected
+			$('#pdf-button').text('Convert to Text')
+			$('#pdf-button').attr('onclick', 'submitFile();')
+			$('#change-pdf').show();
+		})
+		$("#choose-pdf").click();
+	}
+
+	function submitFile() {
+		$('.header').addClass('shrink');
+		$('#pdf-button').addClass('shrink');
+		$('span').addClass('shrink');
+
+		$('#submit-pdf').click();
+		$('#pdf-button').text('Cancel');
+		$('#pdf-button').css('background', '#ea8557');
+		$('#change-pdf').hide();
+	}
+
+	function changePDF() {
+		$('#pdf-button').attr('onclick', 'chooseFile();');
+		$('#pdf-button').click();
+	}
+});
+
+app.config(function($routeProvider) {
+	$routeProvider.when('/', {
+		controller: 'MainCtrl',
+		templateUrl: '/partials/pdf_upload.html'
+	});
+})
+
