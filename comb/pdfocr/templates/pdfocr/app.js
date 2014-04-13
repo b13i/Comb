5e80e546c0281429307f59086d0aaa43def5c03e
Babak Pourkazemi <bp5xj@virginia.edu>
2014-04-13 06:10:35 -0400

    Angular is a work-in-progress. Resorting to uber hacks now

--- comb/pdfocr/templates/pdfocr/app.js
+++ comb/pdfocr/templates/pdfocr/app.js
@@ -3,18 +3,22 @@ var app = angular.module('app', []);
 app.controller('MainCtrl', function($scope) {
 	console.log('Current controller: MainCtrl');
 
-	$scope.chooseFile = function() {
-		$('#choose-pdf').change(function() {
-			// File has now been selected
-			$('#pdf-button').text('Convert to Text')
-			$('#pdf-button').attr('onclick', 'submitFile();')
-			$('#change-pdf').show();
-		})
-		$("#choose-pdf").click();
-	}
+	$scope.submitFile = function((files) {
+	    var fd = new FormData();
+	    // Take the first selected file
+	    fd.append("file", files[0]);
 
-	$scope.submitFile = function() {
-		$('.header').addClass('shrink');
+	    $http.post('/upload/', fd, {
+	        withCredentials: true,
+	        headers: {'Content-Type': undefined },
+	        transformRequest: angular.identity
+	    }).success(function(data, status, headers, config) {
+	    	console.log("SUCCESS");
+	    }).error(function() {
+	    	console.log("ERROR")
+	    });
+
+	    $('.header').addClass('shrink');
 		$('#pdf-button').addClass('shrink');
 		$('span').addClass('shrink');
 
@@ -22,10 +26,32 @@ app.controller('MainCtrl', function($scope) {
 		$('#pdf-button').text('Cancel');
 		$('#pdf-button').css('background', '#ea8557');
 		$('#change-pdf').hide();
+
+	});
+
+	$scope.chooseFile = function() {
+		$('#choose-pdf').change(function() {
+			// File has now been selected
+			$('#pdf-button').text('Convert to Text')
+			$('#pdf-button').attr('ng-click', 'submitFile();')
+			$('#change-pdf').show();
+		})
+		$("#choose-pdf").click();
 	}
 
+	// $scope.submitFile = function() {
+	// 	$('.header').addClass('shrink');
+	// 	$('#pdf-button').addClass('shrink');
+	// 	$('span').addClass('shrink');
+
+	// 	$('#submit-pdf').click();
+	// 	$('#pdf-button').text('Cancel');
+	// 	$('#pdf-button').css('background', '#ea8557');
+	// 	$('#change-pdf').hide();
+	// }
+
 	$scope.changePDF = function() {
-		$('#pdf-button').attr('onclick', 'chooseFile();');
+		$('#pdf-button').attr('ng-click', 'chooseFile();');
 		$('#pdf-button').click();
 	}
 });
--- comb/static/js/app.js
+++ comb/static/js/app.js
@@ -3,6 +3,31 @@ var app = angular.module('app', []);
 app.controller('MainCtrl', function($scope) {
 	console.log('Current controller: MainCtrl');
 
+	$scope.submitFile = function(files) {
+	    var fd = new FormData();
+	    // Take the first selected file
+	    fd.append("file", files[0]);
+
+	    $http.post('/upload/', fd, {
+	        withCredentials: true,
+	        headers: {'Content-Type': undefined },
+	        transformRequest: angular.identity
+	    }).success(function(data, status, headers, config) {
+	    	console.log("SUCCESS");
+	    }).error(function() {
+	    	console.log("ERROR")
+	    });
+
+	    $('.header').addClass('shrink');
+		$('#pdf-button').addClass('shrink');
+		$('span').addClass('shrink');
+
+		$('#submit-pdf').click();
+		$('#pdf-button').text('Cancel');
+		$('#pdf-button').css('background', '#ea8557');
+		$('#change-pdf').hide();
+	};
+
 	$scope.chooseFile = function() {
 		$('#choose-pdf').change(function() {
 			// File has now been selected
@@ -13,16 +38,16 @@ app.controller('MainCtrl', function($scope) {
 		$("#choose-pdf").click();
 	}
 
-	$scope.submitFile = function() {
-		$('.header').addClass('shrink');
-		$('#pdf-button').addClass('shrink');
-		$('span').addClass('shrink');
+	// $scope.submitFile = function() {
+	// 	$('.header').addClass('shrink');
+	// 	$('#pdf-button').addClass('shrink');
+	// 	$('span').addClass('shrink');
 
-		$('#submit-pdf').click();
-		$('#pdf-button').text('Cancel');
-		$('#pdf-button').css('background', '#ea8557');
-		$('#change-pdf').hide();
-	}
+	// 	$('#submit-pdf').click();
+	// 	$('#pdf-button').text('Cancel');
+	// 	$('#pdf-button').css('background', '#ea8557');
+	// 	$('#change-pdf').hide();
+	// }
 
 	$scope.changePDF = function() {
 		$('#pdf-button').attr('onclick', 'chooseFile();');
@@ -31,10 +56,8 @@ app.controller('MainCtrl', function($scope) {
 });
 
 app.config(function($routeProvider) {
-	$routeProvider.when('/index/', {
-		controller: 'MainCtrl',
-		templateUrl: '/partials/pdf_upload.html'
-	})
-	.otherwise({ redirectTo: '/index/' });
+	$routeProvider.when('/', {
+		controller: 'MainCtrl'
+	});
 });
 
