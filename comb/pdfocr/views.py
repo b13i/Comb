3a1004ae76618158464e780112d25105d0695b70
Babak Pourkazemi <bp5xj@virginia.edu>
2014-04-13 06:38:40 +0000

    static files

--- comb/pdfocr/views.py
+++ comb/pdfocr/views.py
@@ -15,26 +15,46 @@ def upload(request):
 		file_contents = request.FILES
 		pdf_name = str(file_contents['user-pdf'])
 		
-		# text_array = runner.pdf_to_text(pdf_name)	
-		# text = ''.join(text_array)
-		# context = { 'text': text }
+		text_array = runner.pdf_to_text(pdf_name)	
+		text = ''.join(text_array)
+		context = { 'text': text }
 
-		png_filepaths = runner.pdf_to_pnglist(pdf_name)
-		return StreamingHttpResponse(request, png_filepaths)
+		# png_filepaths = runner.pdf_to_pnglist(pdf_name)
+		# return StreamingHttpResponse(request, png_filepaths)
+		# return stream_response(request)
+		
 	
-		# if file_contents:
-		# 	return render(request, 'pdfocr/results.html', context)
-		# else:
-		# 	# TODO: Graceful failure
-		# 	return HttpResponse('Failed to upload file. Try again.')
+		if file_contents:
+			return render(request, 'pdfocr/results.html', context)
+		else:
+		 	# TODO: Graceful failure
+		 	return HttpResponse('Failed to upload file. Try again.')
 
 
 
-def stream_response(request, png_filepaths):
-    return StreamingHttpResponse(stream_response_generator(png_filepaths))
+#def stream_response(request, png_filepaths):
+ #   return StreamingHttpResponse(stream_response_generator(png_filepaths))
 
-def stream_response_generator(png_filepaths):
-	for index, path in enumerate(png_filepaths):
-		text = runner.ocr(path)
-		yield "------ PAGE {}------\n{}\n".format(index, text)
-		time.sleep(1)
+#def stream_response_generator(png_filepaths):
+#	print 'HERE2'
+#	yield "<html><body>\n"
+#	for index, path in enumerate(png_filepaths):
+#		text = runner.ocr(path)
+#		print 'CONVERTED ' + text
+#		yield "<div>------ PAGE {}------\n{}\n</div>".format(index, text)
+#		time.sleep(1)
+#	yield "</body></html>\n"
+
+
+#@condition(etag_func=None)
+#def stream_response(request):
+#    resp = HttpResponse( stream_response_generator(), mimetype='text/html')
+ #   return resp
+#
+#def stream_response_generator():
+ #   yield "<html><body>\n"
+  #  for x in range(1,11):
+   #     yield "<div>%s</div>\n" % x
+    #    yield " " * 1024  # Encourage browser to render incrementally
+     #   time.sleep(1)
+    #yield "</body></html>\n"
