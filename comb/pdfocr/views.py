fdda2d703f7afe97c2cdf155e51836576f59d579
Babak Pourkazemi <bp5xj@virginia.edu>
2014-04-13 01:58:46 -0400

    Streaming 4 lyfe

--- comb/pdfocr/views.py
+++ comb/pdfocr/views.py
@@ -15,40 +15,26 @@ def upload(request):
 		file_contents = request.FILES
 		pdf_name = str(file_contents['user-pdf'])
 		
-		text_array = runner.pdf_to_text(pdf_name)	
-		text = ''.join(text_array)
-		context = { 'text': text }
+		# text_array = runner.pdf_to_text(pdf_name)	
+		# text = ''.join(text_array)
+		# context = { 'text': text }
 
 		png_filepaths = runner.pdf_to_pnglist(pdf_name)
-		# return StreamingHttpResponse(request, png_filepaths)
-		return stream_response(request)
+		return StreamingHttpResponse(request, png_filepaths)
 	
 		# if file_contents:
 		# 	return render(request, 'pdfocr/results.html', context)
 		# else:
 		# 	# TODO: Graceful failure
-			# return HttpResponse('Failed to upload file. Try again.')
+		# 	return HttpResponse('Failed to upload file. Try again.')
 
 
 
-# def stream_response(request, png_filepaths):
-#     return StreamingHttpResponse(stream_response_generator(png_filepaths))
+def stream_response(request, png_filepaths):
+    return StreamingHttpResponse(stream_response_generator(png_filepaths))
 
-# def stream_response_generator(png_filepaths):
-# 	for index, path in enumerate(png_filepaths):
-# 		text = runner.ocr(path)
-# 		yield "------ PAGE {}------\n{}\n".format(index, text)
-# 		time.sleep(1)
-
-@condition(etag_func=None)
-def stream_response(request):
-    resp = HttpResponse( stream_response_generator(), mimetype='text/html')
-    return resp
-
-def stream_response_generator():
-    yield "<html><body>\n"
-    for x in range(1,11):
-        yield "<div>%s</div>\n" % x
-        yield " " * 1024  # Encourage browser to render incrementally
-        time.sleep(1)
-    yield "</body></html>\n"
+def stream_response_generator(png_filepaths):
+	for index, path in enumerate(png_filepaths):
+		text = runner.ocr(path)
+		yield "------ PAGE {}------\n{}\n".format(index, text)
+		time.sleep(1)
