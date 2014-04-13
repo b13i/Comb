4232ffa9d22cd44dc824bc63f445f1d91de11541
Babak Pourkazemi <bp5xj@virginia.edu>
2014-04-13 01:30:43 -0400

    Streaming v2

--- comb/pdfocr/views.py
+++ comb/pdfocr/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, StreamingHttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import condition
 from ocr_magic import runner
 
 # Create your views here.
@@ -19,7 +20,8 @@ def upload(request):
 		context = { 'text': text }
 
 		png_filepaths = runner.pdf_to_pnglist(pdf_name)
-		return StreamingHttpResponse(request, png_filepaths)
+		# return StreamingHttpResponse(request, png_filepaths)
+		return stream_response(request)
 	
 		# if file_contents:
 		# 	return render(request, 'pdfocr/results.html', context)
@@ -27,11 +29,26 @@ def upload(request):
 		# 	# TODO: Graceful failure
 			# return HttpResponse('Failed to upload file. Try again.')
 
-def stream_response(request, png_filepaths):
-    return StreamingHttpResponse(stream_response_generator(png_filepaths))
 
-def stream_response_generator(png_filepaths):
-	for index, path in enumerate(png_filepaths):
-		text = runner.ocr(path)
-		yield "------ PAGE {}------\n{}\n".format(index, text)
-		time.sleep(1)
+
+# def stream_response(request, png_filepaths):
+#     return StreamingHttpResponse(stream_response_generator(png_filepaths))
+
+# def stream_response_generator(png_filepaths):
+# 	for index, path in enumerate(png_filepaths):
+# 		text = runner.ocr(path)
+# 		yield "------ PAGE {}------\n{}\n".format(index, text)
+# 		time.sleep(1)
+
+@condition(etag_func=None)
+def stream_response(request):
+    resp = HttpResponse( stream_response_generator(), mimetype='text/html')
+    return resp
+
+def stream_response_generator():
+    yield "<html><body>\n"
+    for x in range(1,11):
+        yield "<div>%s</div>\n" % x
+        yield " " * 1024  # Encourage browser to render incrementally
+        time.sleep(1)
+    yield "</body></html>\n"
