cdb7c4c7c78a48052f8d4881658d99fa1b83696c
Babak Pourkazemi <bp5xj@virginia.edu>
2014-04-13 05:00:32 -0400

    Major visual update

---
+++ comb/static/js/app.js
@@ -0,0 +1,25 @@
+function chooseFile() {
+	$('#choose-pdf').change(function() {
+		// File has now been selected
+		$('#pdf-button').text('Convert to Text')
+		$('#pdf-button').attr('onclick', 'submitFile();')
+		$('#change-pdf').show();
+	})
+	$("#choose-pdf").click();
+}
+
+function submitFile() {
+	$('.header').addClass('shrink');
+	$('#pdf-button').addClass('shrink');
+	$('span').addClass('shrink');
+	
+	$('#submit-pdf').click();
+	$('#pdf-button').text('Cancel');
+	$('#pdf-button').css('background', '#ea8557');
+	$('#change-pdf').hide();
+}
+
+function changePDF() {
+	$('#pdf-button').attr('onclick', 'chooseFile();');
+	$('#pdf-button').click();
+}
